0f25eacc4ef066e67b83ef232947c19f603e2646
ShengWay <m13594156534@gmail.com>
2025-11-11 15:16:26 +0800

    Initial project

---
+++ Source/Demo/DesignPatterns.Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatterns.Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
